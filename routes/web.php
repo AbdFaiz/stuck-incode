3095ea1614aac150d116cb932fd4c84daa27facf
AbdFaiz <abdurrahmanfaiz187@gmail.com>
2024-09-18 21:58:31 +0700

    crud but not final

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\TagController;
+use App\Models\Tag;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -12,5 +15,19 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('posts', PostController::class);
-Route::get('/questions', [App\Http\Controllers\HomeController::class, 'allQuest'])->name('questions');
+Route::middleware('auth')->group(function () {
+
+    Route::resource('posts', PostController::class);
+    Route::get('/questions', [App\Http\Controllers\HomeController::class, 'allQuest'])->name('questions');
+    Route::get('/home', [App\Http\Controllers\PostController::class, 'topQuestions'])->name('home');
+
+    Route::resource('posts', PostController::class);
+    Route::resource('tags', TagController::class);
+    Route::get('/questions', [App\Http\Controllers\PostController::class, 'index'])->name('questions');
+});
+
+Route::get('/api/tags', function (Request $request) {
+    $query = $request->query('query');
+    $tags = Tag::where('name', 'like', "%{$query}%")->limit(5)->get(['name']);
+    return response()->json($tags);
+});
