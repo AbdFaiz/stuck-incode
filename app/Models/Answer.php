bc1d5e0fc5964ad184176794c6262771de76adb4
AbdFaiz <abdurrahmanfaiz187@gmail.com>
2024-09-25 10:27:32 +0700

    membuat pencapaian user

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -12,6 +12,34 @@ class Answer extends Model
     protected $guarded = [];
 
     // Relasi ke pengguna
+    protected static function booted()
+    {
+        static::updated(function ($answer) {
+            if ($answer->is_accepted) {
+                $user = $answer->user;
+                $acceptedAnswersCount = $user->accepted_answers_count;
+
+                // Tambahkan bronze badge jika jawaban yang diterima lebih dari 5
+                if ($acceptedAnswersCount >= 5 && $acceptedAnswersCount < 25 && $user->bronze_badges == 0) {
+                    $user->bronze_badges = 1;
+                }
+
+                // Tambahkan silver badge jika jawaban yang diterima lebih dari 25
+                if ($acceptedAnswersCount >= 25 && $acceptedAnswersCount < 50 && $user->silver_badges == 0) {
+                    $user->silver_badges = 1;
+                }
+
+                // Tambahkan gold badge jika jawaban yang diterima lebih dari 50
+                if ($acceptedAnswersCount >= 50 && $user->gold_badges == 0) {
+                    $user->gold_badges = 1;
+                }
+
+                $user->save();
+            }
+        });
+    }
+
+    // Relasi jawaban dengan pengguna
     public function user()
     {
         return $this->belongsTo(User::class);
